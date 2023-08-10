5fe589d6b00d927a8da592c093920cc87b840a04
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-08-10 00:40:37 -0400

    NavBar CSS-Not staying like that its more of a draft to make app easier to test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,25 @@
+import React from "react";
 import { Link } from "react-router-dom";
+
+import NavBarLogo from "../NavBar-icon.jpeg";
+import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <nav>
-      <h1>
-        <Link to="/Movies">Movies</Link>
-      </h1>
-      <button>
-        <Link to="/Movies/new">Add a Favorite Movie</Link>
-      </button>
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/"> {/* Link to the home page */}
+          <img src={NavBarLogo} alt="NavBar Logo" />
+        </Link>
+      </div>
+      <div className="links">
+        <h1>
+          <Link to="/movies">Movies</Link>
+        </h1>
+        <button>
+          <Link to="/movies/new">Add a Favorite Movie</Link>
+        </button>
+      </div>
     </nav>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import movieClubImage from "../movies-logo-two.png";
 
 export default function Home() {
   return (
-    <div>Welcome to the Movie App!</div>
-  )
+    <div>
+      <h1>Welcome to the Movie App!</h1>
+      <img src={movieClubImage} alt="Movie Club" />
+    </div>
+  );
 }
